e9efd24e98ee270a81b4ce440050de0fb9b402c4
Dylan K. Taylor <odigiman@gmail.com>
2020-10-10 23:02:10 +0100

    expose libdeflate_zlib_compress() and libdeflate_gzip_compress()

--- libdeflate.c
+++ libdeflate.c
@@ -38,42 +38,64 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_libdeflate_compress, 0, 1, IS_ST
 	ZEND_ARG_TYPE_INFO(0, level, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
-/* {{{ proto string libdeflate_deflate_compress(string $data [, int $level = 6])
- * Compresses data in zlib raw format (equivalent to zlib_encode($data, ZLIB_ENCODING_RAW, $level)) */
-PHP_FUNCTION(libdeflate_deflate_compress) {
-	zend_string *data;
-	zend_long level = 6;
+typedef size_t (*php_libdeflate_compress_bound_func)(struct libdeflate_compressor*, size_t);
+typedef size_t (*php_libdeflate_compress_func)(struct libdeflate_compressor*, const void*, size_t, void*, size_t);
 
-	ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 1, 2)
-		Z_PARAM_STR(data)
-		Z_PARAM_OPTIONAL
-		Z_PARAM_LONG(level)
-	ZEND_PARSE_PARAMETERS_END();
-
+static inline zend_string* php_libdeflate_compress(zend_string *data, zend_long level, php_libdeflate_compress_bound_func compressBoundFunc, php_libdeflate_compress_func compressFunc) {
 	struct libdeflate_compressor* compressor = libdeflate_alloc_compressor(level);
 	if (compressor == NULL) {
 		zend_throw_exception_ex(spl_ce_RuntimeException, 0, "Unable to allocate libdeflate compressor");
-		return;
+		return NULL;
 	}
 
-	size_t compressBound = libdeflate_deflate_compress_bound(compressor, ZSTR_LEN(data));
+	size_t compressBound = compressBoundFunc(compressor, ZSTR_LEN(data));
 	void* output = emalloc(compressBound);
-	size_t actualSize = libdeflate_deflate_compress(compressor, ZSTR_VAL(data), ZSTR_LEN(data), output, compressBound);
+	size_t actualSize = compressFunc(compressor, ZSTR_VAL(data), ZSTR_LEN(data), output, compressBound);
 
 	if (actualSize == 0){
 		zend_throw_exception_ex(spl_ce_RuntimeException, 0, "Too small buffer provided (this is a bug)");
-		return;
+		return NULL;
 	}
 
-	RETVAL_STRINGL(output, actualSize);
+	zend_string* result = zend_string_init(output, actualSize, 0);
 	efree(output);
 	libdeflate_free_compressor(compressor);
-} /* }}} */
+	return result;
+}
+
+#define PHP_LIBDEFLATE_FUNC(compressFunc, compressBoundFunc) \
+PHP_FUNCTION(compressFunc) { \
+	zend_string *data; \
+	zend_long level = 6; \
+\
+	ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 1, 2) \
+		Z_PARAM_STR(data) \
+		Z_PARAM_OPTIONAL \
+		Z_PARAM_LONG(level) \
+	ZEND_PARSE_PARAMETERS_END(); \
+\
+	zend_string *result = php_libdeflate_compress(data, level, compressBoundFunc, compressFunc); \
+	if (result == NULL) { \
+		return; \
+	} \
+	RETURN_STR(result); \
+}
+
+/* {{{ proto string libdeflate_deflate_compress(string $data [, int $level = 6]) */
+PHP_LIBDEFLATE_FUNC(libdeflate_deflate_compress, libdeflate_deflate_compress_bound) /* }}} */
+
+/* {{{ proto string libdeflate_zlib_compress(string $data [, int $level = 6]) */
+PHP_LIBDEFLATE_FUNC(libdeflate_zlib_compress, libdeflate_zlib_compress_bound) /* }}} */
+
+/* {{{ proto string libdeflate_gzip_compress(string $data [, int $level = 6]) */
+PHP_LIBDEFLATE_FUNC(libdeflate_gzip_compress, libdeflate_gzip_compress_bound) /* }}} */
 
 /* {{{ libdeflate_functions[]
  */
 static const zend_function_entry libdeflate_functions[] = {
 	PHP_FE(libdeflate_deflate_compress, arginfo_libdeflate_compress)
+	PHP_FE(libdeflate_zlib_compress, arginfo_libdeflate_compress)
+	PHP_FE(libdeflate_gzip_compress, arginfo_libdeflate_compress)
 	PHP_FE_END
 };
 /* }}} */
