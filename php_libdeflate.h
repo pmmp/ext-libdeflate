a751d1391b9e5ad5bc56d4bcd52e5d701d4d29bd
Dylan K. Taylor <dktapps@pmmp.io>
2023-03-13 21:37:13 +0000

    Release 0.2.0

--- php_libdeflate.h
+++ php_libdeflate.h
@@ -6,7 +6,7 @@
 extern zend_module_entry libdeflate_module_entry;
 # define phpext_libdeflate_ptr &libdeflate_module_entry
 
-# define PHP_LIBDEFLATE_VERSION "0.1.0"
+# define PHP_LIBDEFLATE_VERSION "0.2.0"
 
 # if defined(ZTS) && defined(COMPILE_DL_LIBDEFLATE)
 ZEND_TSRMLS_CACHE_EXTERN()
